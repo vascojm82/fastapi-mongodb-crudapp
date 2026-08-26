eef81e8dff44e74ac0fcd74c45b959c2f20f0c99
Jose Vasconcellos <miguel_vasconce@hotmail.com>
2026-08-26 15:03:57 -0400

    second commit

--- index.py
+++ index.py
@@ -1,4 +1,14 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.student import student_router
+
+client_apps= ['http://localhost:3000']  # Our React app will be running on this IP and port
 app = FastAPI()
 app.include_router(student_router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[client_apps],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
